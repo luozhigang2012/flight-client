785d669c4e8db8e592d90766fee46ce481656652
Zhigang Luo <lzgluo@cn.ibm.com>
2025-07-27 15:10:15 +0800

    react-query-devtools & eslint-plugin-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App";
 import { StagewiseToolbar } from "@stagewise/toolbar-react";
@@ -21,6 +22,8 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+      {/* Stagewise Toolbar for debugging */}
       <StagewiseToolbar />
     </QueryClientProvider>
   </StrictMode>
